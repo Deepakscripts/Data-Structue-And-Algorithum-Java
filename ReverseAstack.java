bcf45894a8640ac3bb98c9a31b17cb636375cfd8
Deepak agrawal <aadeepak05@gmail.com>
2024-04-25 10:00:35 +0530

    ok

--- ReverseAstack.java
+++ ReverseAstack.java
@@ -1,6 +1,22 @@
+
 /* REVERSE A STRING USING A STACK */
 import java.util.*;
 
 public class ReverseAstack {
-    
+    public static void main(String[] args) {
+        Stack<Character> s = new Stack<>();
+        String str = "abc";
+        int idx = 0;
+        while (idx < str.length()) {
+            s.push(str.charAt(idx));
+            idx++;
+        }
+        StringBuilder result = new StringBuilder("");
+        while (!s.empty()) {
+            char current = s.pop();
+            result.append(current);
+        }
+        str = result.toString();
+        System.out.println(str);
+    }
 }
